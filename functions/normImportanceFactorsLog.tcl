c9f4dbf06526b449b3637882b56f2ba5cafe6ee8
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2014-01-29 08:30:14 +0100

    Bugfix

--- functions/normImportanceFactorsLog.tcl
+++ functions/normImportanceFactorsLog.tcl
@@ -4,8 +4,11 @@ global samples_k numSamples
   # set the new importanceFactor
   for {set kk 0} {$kk < $numSamples} {incr kk} {
     # Calculate the real probability
-    if {[catch {set importanceFactor [expr exp([lindex $samples_k($kk) 3] - $alphaLog)]}]} {
-	  set importanceFactor 0.0
+	set importanceFactorLog [ expr [lindex $samples_k($kk) 3] - $alphaLog ]
+	if { ($importanceFactorLog == -inf) || ($importanceFactorLog < -500) } {
+	   set importanceFactor 0.0
+	} else {
+	   set importanceFactor [ expr exp($importanceFactorLog) ]
 	}
 	set samples_k($kk) [list [lindex $samples_k($kk) 0] [lindex $samples_k($kk) 1] [lindex $samples_k($kk) 2] $importanceFactor] 
   }
