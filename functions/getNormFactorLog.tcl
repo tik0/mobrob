42d2100a152d4a2f61f301190e2f222d5b06f45f
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2014-01-29 08:30:54 +0100

    Bugfix

--- functions/getNormFactorLog.tcl
+++ functions/getNormFactorLog.tcl
@@ -1,8 +1,8 @@
 proc getNormFactorLog {} {
 global samples_k numSamples
-  set alpha 0.0
+  set alphaLog -inf
   for {set kk 0} {$kk < $numSamples} {incr kk} {
-	set alpha [ logAdd $alpha [ lindex $samples_k($kk) 3 ] ]
+	set alphaLog [ logAdd $alphaLog [ lindex $samples_k($kk) 3 ] ]
   }
-  return $alpha
+  return $alphaLog
 }
